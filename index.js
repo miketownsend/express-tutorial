96bd4f58ded011bf92faf9fa9c86d4abc93cbdb2
Michael Townsend <mike@villageinteractive.co.nz>
2017-09-01 17:23:13 +1200

    ZEPH-79 Created instrument route, data and thumbnail components

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express'),
 	bodyParser = require('body-parser'),
   path = require('path'),
   restRouter = require('./rest'),
+  fs = require('fs'),
   instruments = require('./data/instruments');
 
 const app = express();
@@ -15,12 +16,22 @@ app.use(function (req, res, next) {
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse text
+app.use(bodyParser.text({ type: 'text/*' }));
+app.use(bodyParser.text({ type: 'application/xml' }));
+
 // parse application/json
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
 	req.user = { name: "mike" };
 	next();
+})
+
+app.use('/nav_test', function (req, res) {
+	console.log(req.body);
+	fs.writeFileSync('./export-output.xml', req.body);
+	res.send(200)
 })
 
 app.use('/resources', function (req, res) {
